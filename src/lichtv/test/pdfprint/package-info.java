0edcff58ebe2607f3f05db8e064d8e2edf6cdb2d
tvlictnewb <tvlictnewb@gmail.com>
2015-08-09 08:24:36 +0700

    FVS

---
+++ src/lichtv/test/pdfprint/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @create date : Aug 8, 2015
+ * @copyright Lich Truong Van - lichtv
+ * @author lichtv - Software Engineer / Computer Science
+ * @contact : 1/Email : lichcse@gmail.com  2/Skype : lichcse  3/Mobile : (+84)1677897159
+ */
+/**
+ * Description File.
+ * 
+ */
+package lichtv.test.pdfprint;
---
+++ src/lichtv/test/trivial/TestFindBug.java
@@ -0,0 +1,26 @@
+/**
+ * @create date : Aug 8, 2015
+ * @copyright Lich Truong Van - lichtv
+ * @author lichtv - Software Engineer / Computer Science
+ * @contact : 1/Email : lichcse@gmail.com  2/Skype : lichcse  3/Mobile : (+84)1677897159
+ */
+package lichtv.test.trivial;
+
+/**
+ * Description File.
+ * 
+ */
+public class TestFindBug {
+
+	/**
+	 * Constructor
+	 */
+	public TestFindBug() {
+		//String str = "Truong Van Lich";
+		// Call to String.equals(Character) in new lichtv.test.trivial.TestFindBug() [Scariest(1), High confidence]
+		// if(str.equals('l')){
+		// System.out.println("OK");
+		// }
+	}
+
+}
